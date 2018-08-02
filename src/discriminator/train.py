ee53548d27a8fffcb4bbbe58b42144a2590aa5c4
Tom Hosking <tomhosking@gmail.com>
2018-08-02 15:15:46 +0100

    Fix disc trainer for squad

--- src/discriminator/train.py
+++ src/discriminator/train.py
@@ -15,12 +15,11 @@ def main(_):
 
     FLAGS = tf.app.flags.FLAGS
 
-    with open('./logs'+'/out_eval_'+ FLAGS.disc_modelslug +'.json') as f:
-        results = json.load(f)
+
 
     # results=results[:32]
 
-    squad_v2 = loader.load_squad_triples(FLAGS.data_path, False, v2=True)
+
 
     # dev_ctxts, dev_qs,dev_ans,dev_ans_pos, dev_correct = zip(*squad_dev)
 
@@ -28,12 +27,15 @@ def main(_):
     negative_data=[]
 
     if FLAGS.disc_trainongenerated is True:
+        with open('./logs'+'/out_eval_'+ FLAGS.disc_modelslug +'.json') as f:
+            results = json.load(f)
         for res in results:
             qpred,qgold,ctxt,ans_text,ans_pos =res
             positive_data.append( (ctxt, qgold, ans_text, ans_pos) )
             negative_data.append( (ctxt, qpred, ans_text, ans_pos) )
 
     if FLAGS.disc_trainonsquad is True:
+        squad_v2 = loader.load_squad_triples(FLAGS.data_path, False, v2=True)
         for res in squad_v2:
             ctxt,q,ans_text,ans_pos,label =res
             if label is True:
@@ -46,9 +48,16 @@ def main(_):
     disc = DiscriminatorInstance(trainable=True, log_slug=FLAGS.disc_modelslug)
     # disc.load_from_chkpt() # this loads the embeddings etc
 
+    train_samples = math.floor(0.8*num_instances)
+    dev_samples = math.floor(0.2*num_instances)
 
-    num_steps_train = math.floor(0.8*num_instances)//FLAGS.batch_size
-    num_steps_dev = math.floor(0.2*num_instances)//FLAGS.batch_size
+    positive_data_train = positive_data[:train_samples]
+    negative_data_train = negative_data[:train_samples]
+    positive_data_dev = positive_data[train_samples:]
+    negative_data_dev = negative_data[train_samples:]
+
+    num_steps_train = train_samples//FLAGS.batch_size
+    num_steps_dev = dev_samples//FLAGS.batch_size
     num_steps_squad = num_steps_dev
 
     best_oos_nll=1e6
@@ -60,7 +69,7 @@ def main(_):
         for i in tqdm(range(num_steps_train), desc='Epoch '+str(e)):
             ixs = np.round(np.random.binomial(1,0.5,FLAGS.batch_size))
             # batch = train_data[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size]
-            batch = [negative_data[i*FLAGS.batch_size+j] if ix < 0.5 else positive_data[i*FLAGS.batch_size+j] for j,ix in enumerate(ixs.tolist())]
+            batch = [negative_data_train[i*FLAGS.batch_size+j] if ix < 0.5 else positive_data_train[i*FLAGS.batch_size+j] for j,ix in enumerate(ixs.tolist())]
             ctxt,qbatch,ans_text,ans_pos = zip(*batch)
 
             # print(ans_text)
@@ -80,17 +89,12 @@ def main(_):
         dev_acc=[]
         dev_nll=[]
         for i in tqdm(range(num_steps_dev), desc='Epoch '+str(e) + " dev"):
-            batch = dev_data[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size]
-            qpred,qgold,ctxt,ans_text,ans_pos = zip(*batch)
-
-            # print(ans_text)
-            # print(ans_pos)
-            # print(ctxt)
-            # exit()
 
             ixs = np.round(np.random.binomial(1,0.5,FLAGS.batch_size))
+            batch = [negative_data_train[i*FLAGS.batch_size+j] if ix < 0.5 else positive_data_train[i*FLAGS.batch_size+j] for j,ix in enumerate(ixs.tolist())]
+            ctxt,qbatch,ans_text,ans_pos = zip(*batch)
 
-            qbatch = [qpred[ix].replace("</Sent>","").replace("<PAD>","") if ixs[ix] < 0.5 else qgold[ix] for ix in range(FLAGS.batch_size)]
+            qbatch = [q.replace(" </Sent>","").replace(" <PAD>","") for q in qbatch]
 
             pred = disc.get_pred(ctxt, qbatch, ans_text, ans_pos)
             nll = disc.get_nll(ctxt, qbatch, ans_text, ans_pos, ixs)
@@ -112,30 +116,7 @@ def main(_):
             disc.save_to_chkpt(FLAGS.model_dir, e)
             print("New best NLL, saving")
 
-        squad_acc=[]
-        squad_nll=[]
-        squad_ixs=[]
-        np.random.shuffle(squad_dev)
-        for i in tqdm(range(num_steps_squad), desc='Epoch '+str(e)+ " squad"):
-            batch = squad_dev[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size]
-            ctxt,qbatch,ans_text,ans_pos,labels = zip(*batch)
 
-            # print(ans_text)
-            # print(ans_pos)
-            # print(ctxt)
-            # exit()
-
-            # ixs = np.round(np.random.binomial(1,0.5,FLAGS.batch_size))
-
-            # qbatch = [qpred[ix].replace("</Sent>","").replace("<PAD>","") if ixs[ix] < 0.5 else qgold[ix] for ix in range(FLAGS.batch_size)]
-
-            pred = disc.get_pred(ctxt, qbatch, ans_text, ans_pos)
-            nll = disc.get_nll(ctxt, qbatch, ans_text, ans_pos, labels)
-            acc = 1.0*np.equal(np.round(pred), labels)
-            squad_acc.extend(acc)
-            squad_nll.extend(nll)
-            squad_ixs.extend(labels)
-        print("Squad: ", np.mean(squad_acc), " (bias ",np.mean(squad_ixs),")")
 
 if __name__ == "__main__":
     tf.app.run()
