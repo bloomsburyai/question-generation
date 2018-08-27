ba32b5fd2a964a60e1aeed77c787ea2b82fdbfb8
Tom Hosking <tomhosking@gmail.com>
2018-08-27 15:40:57 +0100

    Save disc every 1000 steps

--- src/discriminator/train.py
+++ src/discriminator/train.py
@@ -66,62 +66,64 @@ def main(_):
 
     best_oos_nll=1e6
 
-    for e in range(FLAGS.disc_num_epochs):
-        np.random.shuffle(positive_data_train)
-        np.random.shuffle(negative_data_train)
-        # Train for one epoch
-        for i in tqdm(range(num_steps_train), desc='Epoch '+str(e)):
-            ixs = np.round(np.random.binomial(1,0.5,FLAGS.batch_size))
-            # batch = train_data[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size]
-            batch = [negative_data_train[i*FLAGS.batch_size+j] if ix < 0.5 else positive_data_train[i*FLAGS.batch_size+j] for j,ix in enumerate(ixs.tolist())]
-            ctxt,qbatch,ans_text,ans_pos = zip(*batch)
 
 
-            # print(ixs)
-            # print(qbatch)
-            # print(ans_text)
-            # print(ans_pos)
-            # print(ctxt)
-            # exit()
+    for i in tqdm(range(num_steps_train*FLAGS.disc_num_epochs), desc='Training'):
+        if i % num_steps_train ==0:
+            np.random.shuffle(positive_data_train)
+            np.random.shuffle(negative_data_train)
+        ixs = np.round(np.random.binomial(1,0.5,FLAGS.batch_size))
+        # batch = train_data[i*FLAGS.batch_size:(i+1)*FLAGS.batch_size]
+        batch = [negative_data_train[(i% num_steps_train)*FLAGS.batch_size+j] if ix < 0.5 else positive_data_train[(i% num_steps_train)*FLAGS.batch_size+j] for j,ix in enumerate(ixs.tolist())]
+        ctxt,qbatch,ans_text,ans_pos = zip(*batch)
+
+
+        # print(ixs)
+        # print(qbatch)
+        # print(ans_text)
+        # print(ans_pos)
+        # print(ctxt)
+        # exit()
 
 
 
 
-            # +qpred[ix].replace("</Sent>","").replace("<PAD>","")
-            qbatch = [q.replace(" </Sent>","").replace(" <PAD>","") for q in qbatch]
-            # qbatch = ["fake " if ixs[ix] < 0.5 else "real " for ix in range(FLAGS.batch_size)]
-            # print(qbatch, ixs)
-            loss = disc.train_step(ctxt, qbatch, ans_text, ans_pos, ixs, (e*num_steps_train+i))
+        # +qpred[ix].replace("</Sent>","").replace("<PAD>","")
+        qbatch = [q.replace(" </Sent>","").replace(" <PAD>","") for q in qbatch]
+        # qbatch = ["fake " if ixs[ix] < 0.5 else "real " for ix in range(FLAGS.batch_size)]
+        # print(qbatch, ixs)
+        loss = disc.train_step(ctxt, qbatch, ans_text, ans_pos, ixs, (i))
 
-        dev_acc=[]
-        dev_nll=[]
-        for i in tqdm(range(num_steps_dev), desc='Epoch '+str(e) + " dev"):
+        if i % 1000 == 0 and i >0:
+            dev_acc=[]
+            dev_nll=[]
+            for dev_i in tqdm(range(num_steps_dev), desc='Step '+str(i) + " dev"):
 
-            ixs = np.round(np.random.binomial(1,0.5,FLAGS.batch_size))
-            batch = [negative_data_dev[i*FLAGS.batch_size+j] if ix < 0.5 else positive_data_dev[i*FLAGS.batch_size+j] for j,ix in enumerate(ixs.tolist())]
-            ctxt,qbatch,ans_text,ans_pos = zip(*batch)
+                ixs = np.round(np.random.binomial(1,0.5,FLAGS.batch_size))
+                batch = [negative_data_dev[dev_i*FLAGS.batch_size+j] if ix < 0.5 else positive_data_dev[dev_i*FLAGS.batch_size+j] for j,ix in enumerate(ixs.tolist())]
+                ctxt,qbatch,ans_text,ans_pos = zip(*batch)
 
-            qbatch = [q.replace(" </Sent>","").replace(" <PAD>","") for q in qbatch]
+                qbatch = [q.replace(" </Sent>","").replace(" <PAD>","") for q in qbatch]
 
-            pred = disc.get_pred(ctxt, qbatch, ans_text, ans_pos)
-            nll = disc.get_nll(ctxt, qbatch, ans_text, ans_pos, ixs)
-            acc = 1.0*np.equal(np.round(pred), ixs)
-            dev_acc.extend(acc.tolist())
-            dev_nll.extend(nll.tolist())
+                pred = disc.get_pred(ctxt, qbatch, ans_text, ans_pos)
+                nll = disc.get_nll(ctxt, qbatch, ans_text, ans_pos, ixs)
+                acc = 1.0*np.equal(np.round(pred), ixs)
+                dev_acc.extend(acc.tolist())
+                dev_nll.extend(nll.tolist())
 
-        accsummary = tf.Summary(value=[tf.Summary.Value(tag="dev_perf/acc",
-                                         simple_value=np.mean(dev_acc))])
-        nllsummary = tf.Summary(value=[tf.Summary.Value(tag="dev_perf/nll",
-                                  simple_value=np.mean(dev_nll))])
+            accsummary = tf.Summary(value=[tf.Summary.Value(tag="dev_perf/acc",
+                                             simple_value=np.mean(dev_acc))])
+            nllsummary = tf.Summary(value=[tf.Summary.Value(tag="dev_perf/nll",
+                                      simple_value=np.mean(dev_nll))])
 
-        disc.summary_writer.add_summary(accsummary, global_step=(e+1)*num_steps_train)
-        disc.summary_writer.add_summary(nllsummary, global_step=(e+1)*num_steps_train)
+            disc.summary_writer.add_summary(accsummary, global_step=i)
+            disc.summary_writer.add_summary(nllsummary, global_step=i)
 
-        print(np.mean(dev_acc))
-        if np.mean(dev_nll) < best_oos_nll:
-            best_oos_nll=np.mean(dev_nll)
-            disc.save_to_chkpt(FLAGS.model_dir, e)
-            print("New best NLL, saving")
+            print(np.mean(dev_acc))
+            if np.mean(dev_nll) < best_oos_nll:
+                best_oos_nll=np.mean(dev_nll)
+                disc.save_to_chkpt(FLAGS.model_dir, i)
+                print("New best NLL, saving")
 
 
 
