3c9b890c5ca4548c1b70c05ed715f463412e98d6
Tom Hosking <tomhosking@gmail.com>
2018-09-06 15:43:27 +0100

    Update demo model

--- src/demo/app.py
+++ src/demo/app.py
@@ -5,8 +5,9 @@ sys.path.insert(0, "/home/tomhosking/webapps/qgen/qgen/src/")
 
 import tensorflow as tf
 import numpy as np
-from seq2seq_model import Seq2SeqModel
-from maluuba_model import MaluubaModel
+
+from instance import AQInstance
+
 from helpers import preprocessing
 
 import json
@@ -16,36 +17,8 @@ FLAGS = tf.app.flags.FLAGS
 
 mem_limit=0.9
 
-class AQInstance():
-    def __init__(self, vocab):
-        self.model = Seq2SeqModel(vocab, training_mode=False)
-        # self.model = MaluubaModel(vocab, training_mode=False)
-        with self.model.graph.as_default():
-            self.model.ping = tf.constant("ack")
-        # self.model = MaluubaModel(vocab, training_mode=False)
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=mem_limit,allow_growth = True,visible_device_list='0')
-        self.sess = tf.Session(graph=self.model.graph, config=tf.ConfigProto(gpu_options=gpu_options,allow_soft_placement=True))
 
-    def load_from_chkpt(self, path):
-        self.chkpt_path = path
-        with self.model.graph.as_default():
-            saver = tf.train.Saver()
-            saver.restore(self.sess, tf.train.latest_checkpoint(path))
-            print("Loaded model from "+path)
-
-    def get_q(self, context, ans,ans_pos):
-        # Process and create a batch of 1
-        ctxt_dict = tuple([np.asarray([x]) for x in preprocessing.process_squad_context(self.model.vocab, context_as_set=FLAGS.context_as_set)(context)])
-        ans_dict = tuple([np.asarray([x]) for x in preprocessing.process_squad_answer(self.model.vocab, context_as_set=FLAGS.context_as_set)(ans,ans_pos,context)])
-
-        q,q_len = self.sess.run([self.model.q_hat_beam_string,self.model.q_hat_beam_lens], feed_dict={self.model.context_in: ctxt_dict, self.model.answer_in: ans_dict})
-        q_str = " ".join([w.decode().replace('>','&gt;').replace('<','&lt;') for w in q[0][:q_len[0]-1]])
-        return q_str
-
-    def ping(self):
-        return self.sess.run(self.model.ping)
-
-model_slug_list = ['SEQ2SEQ/1533280948','qgen-s2s-filt1']
+model_slug_list = ['MALUUBA-CROP-SET-GLOVE/1535568489','SEQ2SEQ/1533280948','qgen-s2s-filt1']
 model_slug_curr = model_slug_list[0]
 
 from flask import Flask, current_app, request, redirect
