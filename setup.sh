1eaaf85d6dbcdd6211438188ade13af9a5d9d360
Tom Hosking <tomhosking@gmail.com>
2018-12-13 20:29:02 +0000

    Update setup with new model

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
+mkdir ./models
+mkdir ./models/qgen
+
 curl https://nlp.stanford.edu/data/glove.6B.zip -o ./data/glove.6B.zip -L -C -
 curl https://raw.githubusercontent.com/tomhosking/squad-du-split/master/train-v1.1.json -o ./data/train-v1.1.json -L -C -
 curl https://raw.githubusercontent.com/tomhosking/squad-du-split/master/dev-v1.1.json -o ./data/dev-v1.1.json -L -C -
 curl https://raw.githubusercontent.com/tomhosking/squad-du-split/master/test-v1.1.json -o ./data/test-v1.1.json -L -C -
-mkdir ./models
-mkdir ./models/qgen
-curl https://www.dropbox.com/s/x199un20y6pv7dw/MALUUBA-CROP-SET-GLOVE.zip?dl=0 -o ./models/qgen/MALUUBA-CROP-SET-GLOVE.zip -L -C -
-unzip ./models/qgen/MALUUBA-CROP-SET-GLOVE.zip -d ./models/qgen
+
+curl https://www.dropbox.com/s/l2ne2fe4wqm0buq/RL-S2S-1544356761.zip?dl=0 -o ./models/qgen/RL-S2S-1544356761.zip -L -C -
+
+unzip ./models/qgen/RL-S2S-1544356761.zip -d ./models/qgen
 unzip ./data/glove.6B.zip -d ./data/glove.6B/
+
 python -m nltk.downloader punkt
 python -m spacy download en
 # rm ./data/glove.6B.zip
-rm ./models/qgen/MALUUBA-CROP-SET-GLOVE.zip
+rm ./models/qgen/RL-S2S-1544356761.zip
--- src/demo/app.py
+++ src/demo/app.py
@@ -76,4 +76,4 @@ def init():
 if __name__ == '__main__':
     init()
     with app.app_context():
-        app.run(port=8000)
+        app.run(host="0.0.0.0", port=5000)
